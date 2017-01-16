1ed4d4c9cdfb36218101b37db4d8ad373494bf6f
perlancar (@pc-office) <perlancar@gmail.com>
2017-01-16 19:07:33 +0700

    debian-dist2deb: Add --use-allpackages option

--- lib/App/lcpan/Cmd/debian_dist2deb.pm
+++ lib/App/lcpan/Cmd/debian_dist2deb.pm
@@ -29,6 +29,16 @@ _
             summary => 'Check each distribution if its Debian package exists, using Dist::Util::Debian::dist_has_deb',
             schema => 'bool*',
         },
+        use_allpackages => {
+            summary => 'Will be passed to Dist::Util::Debian::dist_has_deb',
+            description => <<'_',
+
+Using this option is faster if you need to check existence for many Debian
+packages. See <pm:Dist::Util::Debian> documentation for more details.
+
+_
+            schema => 'bool*',
+        },
     },
 };
 sub handle_cmd {
@@ -44,11 +54,14 @@ sub handle_cmd {
 
     push @fields, "exists" if $args{check_exists};
 
+    my $opts = {};
+    $opts->{use_allpackages} = 1 if $args{use_allpackages};
+
     for my $dist (@{ $args{dists} }) {
         my $deb = Dist::Util::Debian::dist2deb($dist);
         my $row = {dist => $dist, deb => $deb};
         if ($args{check_exists}) {
-            $row->{exists} = Dist::Util::Debian::deb_exists($deb);
+            $row->{exists} = Dist::Util::Debian::deb_exists($opts, $deb);
         }
         push @rows, $row;
     }
